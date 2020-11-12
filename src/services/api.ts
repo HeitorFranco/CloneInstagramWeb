83ab38c39e6c9f996822c26c382f1e1a9ee436c3
HeitorFranco <heitorfranco04@gmail.com>
2020-11-12 20:05:07 -0300

    heroku config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-instagram-heitormaf.herokuapp.com/",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     Authorization:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA1MjIwMzI0LCJleHAiOjE2MDUzMDY3MjR9.Z7NKlEDq8Okfpsir5XVVCoBP-48N_WIZISSzlQA3r1Q",
