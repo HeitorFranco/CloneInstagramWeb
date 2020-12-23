c0b665291ad203271984216199a3b9805b005370
HeitorFranco <heitorfranco04@gmail.com>
2020-12-23 01:11:51 -0300

    feat(userdetail): create skeleton of the UserDetail component

---
+++ src/pages/UserDetails/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const UserDetails: React.FC = () => {
+  return (
+    <Container>
+      <h1>UserDetails</h1>
+    </Container>
+  );
+};
+
+export default UserDetails;
